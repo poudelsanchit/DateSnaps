73474ad06a1856e46d464c35940a2b2807e0285b
“Nabin <nabinkandel@student.pu.edu.np>
2023-11-25 09:30:12 +0545

    fix: :bug: date fix

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
-import data from '../data';
-import Draft from '../Draft';
 
 const Card = ({ card }) => {
-    console.log(card)
-
     console.log(card);
-    console.log(card.color)
 
 
-    // var date = card.date.split(" ");
+            if (card && card.date) {
+            const dateObject = new Date(card.date);
+        
+            // Extract year and month
+            var year = dateObject.getFullYear();
+            var month = dateObject.toLocaleString('default', { month: 'long' });
+            var day = dateObject.getDate();
+            }
+  
 
     return (
 
         <div className='w-[285px]  flex flex-col  text-base font-Sans-serif font-medium tracking-tighter	'>
             <div className='flex flex-col items-center border-b-[1px] border-border h-max  w-full pb-4	'>
                 <div className='font-meduim'>
-                    Nov 16
-                    {/* {date[0] + " " + date[1]} */}
-
+                 {month} {day}
                 </div>
-                <div className='text-xs text-datessecondary'>2023</div>
+                <div className='text-xs text-datessecondary'>{year}</div>
 
             </div>
             <div className='h-full border-r-[1px] border-border pl-1 pr-1 pt-2 '>
                 <div>
                     <div className="  rounded-md pt-[6px] px-[14px] pb-[12px] m-[4px]" style={card.color?{backgroundColor:card.color}:null} >
-                        <div className="title">{card?.text?.title}</div>
-                        <div className="date">{card.subdate}</div>
+                        <div className="title">{card.title}</div>
+                        <div className="date">{month} {day}</div>
                         <div className="image"><img className='img' src={card.img} /></div>
                     </div>
                 </div>
