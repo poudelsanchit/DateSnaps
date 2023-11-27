ce1d24da5f307112e2343a8b4c2a66e8d685d09e
Sanchit Poudel <snchtpdl12@gmail.com>
2023-11-27 20:33:00 +0545

    feat: :sparkles: Scroll bar

--- src/Components/ScrollBar.jsx
+++ src/Components/ScrollBar.jsx
@@ -1,10 +1,25 @@
 
 import React from 'react'
-
+import { motion,useScroll, useSpring } from 'framer-motion'
 const ScrollBar = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
-    <div className='  fixed bottom-10 right-[30rem]  h-10 w-[40rem] bg-[#dee1da]  shadow-2xl rounded-full  flex justify-center items-center'>
-     
+    <div className='  fixed bottom-10 right-[30rem]  h-3 w-[30rem] bg-[#dee1da]  shadow-2xl rounded-full  flex justify-center items-center'>
+     <div className='relative w-full h-full'>
+     <motion.div
+        className="progress-bar rounded-full h-full origin-[0] absolute bg-black left-0 right-0"
+        style={{  scaleX }}
+      >
+        </motion.div>
+     </div>
+      
+
     </div>
   )
 }
--- src/PhotoTest.jsx
+++ src/PhotoTest.jsx
@@ -3,39 +3,46 @@ import Image1 from "/images/1.jpg"
 import Image2 from "/images/2.jpg"
 import Image3 from "/images/3.jpg"
 import Image4 from "/images/4.jpg"
-
-import { motion } from 'framer-motion'
+import { motion,useScroll } from 'framer-motion'
 const PhotoTest = () => {
     const [isClicked, setIsClicked] = useState(false);
+    const { scrollYProgress } = useScroll();
+
     const triggerClicked = () => {
         setIsClicked(prev => !prev)
     }
     return (
-        <div className='w-full h-screen flex justify-center items-center  '>
-            <div className={!isClicked ? 'flex h-[50rem] w-[50rem] justify-center items-center origin-center cursor-pointer hover:scale-[.99]  relative ' : 'flex h-[30rem] w-[30rem] justify-center items-center origin-center cursor-pointer hover:scale-[.99]  relative '} onClick={triggerClicked}>
+        // <div className='w-full h-screen flex justify-center items-center  '>
+        //     <div className={!isClicked ? 'flex h-[50rem] w-[50rem] justify-center items-center origin-center cursor-pointer hover:scale-[.99]  relative ' : 'flex h-[30rem] w-[30rem] justify-center items-center origin-center cursor-pointer hover:scale-[.99]  relative '} onClick={triggerClicked}>
 
-                <img src={Image1} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center -rotate-12` : `absolute object-cover  h-40 w-52 left-0 top-0 rounded-sm bg-yellow-700 origin-center -rotate-6 scale-[2] transition-all z-0`} />
+        //         <img src={Image1} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center -rotate-12` : `absolute object-cover  h-40 w-52 left-0 top-0 rounded-sm bg-yellow-700 origin-center -rotate-6 scale-[2] transition-all z-0`} />
 
-                <img src={Image2} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center -rotate-6` : `absolute object-cover  h-40 w-52 right-0 top-0 rounded-sm bg-yellow-700 origin-center rotate-6 scale-[2] transition-all z-0`} />
+        //         <img src={Image2} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center -rotate-6` : `absolute object-cover  h-40 w-52 right-0 top-0 rounded-sm bg-yellow-700 origin-center rotate-6 scale-[2] transition-all z-0`} />
                
-                <img src={Image4} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center rotate-12` : `absolute object-cover  h-40 w-52 left-0 bottom-10 rounded-sm bg-yellow-700 origin-center rotate- scale-[2] transition-all`} />
+        //         <img src={Image4} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center rotate-12` : `absolute object-cover  h-40 w-52 left-0 bottom-10 rounded-sm bg-yellow-700 origin-center rotate- scale-[2] transition-all`} />
                  
-                <img src={Image3} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center rotate-6` : `absolute object-cover  h-40 w-52 right-0 rounded-sm bg-yellow-700 origin-center -rotate-3 scale-[2] transition-all z-40 bottom-10`} />
+        //         <img src={Image3} alt="" className={!isClicked ? `absolute object-cover  h-40 w-40 rounded-sm bg-yellow-700 origin-center rotate-6` : `absolute object-cover  h-40 w-52 right-0 rounded-sm bg-yellow-700 origin-center -rotate-3 scale-[2] transition-all z-40 bottom-10`} />
 
-            </div>
-
-
-
-            {/* <img src={Image1} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center -rotate-12' />
-                <img src={Image2} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center -rotate-6' />
-                <img src={Image3} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center -rotate-0' />
-                <img src={Image4} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center rotate-6' /> */}
+        //     </div>
 
 
 
+        //     {/* <img src={Image1} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center -rotate-12' />
+        //         <img src={Image2} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center -rotate-6' />
+        //         <img src={Image3} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center -rotate-0' />
+        //         <img src={Image4} alt="" className=' object-cover w-96 h-96 rounded-md absolute  origin-center rotate-6' /> */}
 
 
-        </div>
+
+
+
+        // </div>
+        <>
+       
+      <h1>
+        <code>useScroll</code> demo
+      </h1>
+       </>
     )
 }
 
